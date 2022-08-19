670dc1b765cb637934924d93112a508975144e97
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2022-08-19 02:57:16 +0200

    partly example is already working again. What still needs to be achieved to have the log method as this defined when the module is loaded

--- testmosquitto.py
+++ testmosquitto.py
@@ -1,19 +1,25 @@
+from mosquitto_pyplugin import ffi
 from mosquitto_pyplugin import lib as mosquitto_pyplugin
+import sys
+
+def log(loglevel, message):
+    cstr = ffi.new("char[]", message.encode('UTF8'))
+    mosquitto_pyplugin.Log(loglevel, cstr)
 
 def plugin_init(opts):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'plugin_init (opts: %r)' % (opts,)
     )
 
 
 def plugin_cleanup():
-    mosquitto_pyplugin.Log(mosquitto_pyplugin.MOSQ_LOG_DEBUG, 'plugin_cleanup')
+    log(mosquitto_pyplugin.MOSQ_LOG_INFO, 'plugin_cleanup')
 
 
 def unpwd_check(username, password):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'unpwd_check (username: %s password: %s)' % (username, password)
     )
 
@@ -21,26 +27,26 @@ def unpwd_check(username, password):
 
 
 def acl_check(client_id, username, topic, access, payload):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'acl_check %r' % (mosquitto_pyplugin.topic_matches_sub('/#', topic))
     )
 
     if access == mosquitto_pyplugin.MOSQ_ACL_READ:
-        mosquitto_pyplugin.Log(
-            mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+        log(
+            mosquitto_pyplugin.MOSQ_LOG_INFO,
             'acl_check READ (client_id: {} username: {} topic: {} access: {}, payload: {!r})'
             .format(client_id, username, topic, access, payload)
         )
     elif access == mosquitto_pyplugin.MOSQ_ACL_SUBSCRIBE:
-        mosquitto_pyplugin.Log(
-            mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+        log(
+            mosquitto_pyplugin.MOSQ_LOG_INFO,
             'acl_check SUBSCRIBE (client_id: {} username: {} topic: {} access: {}, payload: {!r})'
             .format(client_id, username, topic, access, payload)
         )
     elif access == mosquitto_pyplugin.MOSQ_ACL_WRITE:
-        mosquitto_pyplugin.Log(
-            mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+        log(
+            mosquitto_pyplugin.MOSQ_LOG_INFO,
             'acl_check WRITE (client_id: {} username: {} topic: {} access: {}, payload: {!r})'
             .format(client_id, username, topic, access, payload)
         )
@@ -48,22 +54,22 @@ def acl_check(client_id, username, topic, access, payload):
 
 
 def psk_key_get(identity, hint):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'psk_key_get (identity: %s hint: %s)' % (identity, hint)
     )
     return '0123456789'
 
 
 def security_init(opts, reload):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'security_init (reload: %s, opts: %s)' % (reload, opts)
     )
 
 
 def security_cleanup(reload):
-    mosquitto_pyplugin.Log(
-        mosquitto_pyplugin.MOSQ_LOG_DEBUG,
+    log(
+        mosquitto_pyplugin.MOSQ_LOG_INFO,
         'security_cleanup (reload: %s)' % (reload)
     )
